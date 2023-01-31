bc4a4e715c3a658c9190d4ba6a9fb6f0256af452
yang <yang186107321@163.com>
2023-01-31 16:48:44 +0800

    修改命令执行

--- windows-docker/Controller/DockerPush.go
+++ windows-docker/Controller/DockerPush.go
@@ -1,7 +1,9 @@
 package Controller
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -21,7 +23,7 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	harborurl := strings.Replace(imageurl, "192.168.48.36", "172.20.4.89:8899", -1)
 	//
 	Command := fmt.Sprintf("docker pull %s ", imageurl)
-	output, err := exec.Command("cmd", "-c", Command).Output()
+	output, err := Cmd("cmd", "-c", Command).Output()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -29,7 +31,7 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	fmt.Println(string(output))
 	//
 	Command = fmt.Sprintf("docker tag %s  %s", imageurl, harborurl)
-	output, err = exec.Command("cmd", "-c", Command).Output()
+	output, err = Cmd("cmd", "-c", Command).Output()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -37,7 +39,7 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	fmt.Println(string(output))
 	//
 	Command = fmt.Sprintf("docker push %s", harborurl)
-	output, err = exec.Command("cmd", "-c", Command).Output()
+	output, err = Cmd("cmd", "-c", Command).Output()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -45,7 +47,7 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	fmt.Println(string(output))
 	//
 	Command = fmt.Sprintf("docker rmi %s", harborurl)
-	output, err = exec.Command("cmd", "-c", Command).Output()
+	output, err = Cmd("cmd", "-c", Command).Output()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -53,10 +55,24 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	fmt.Println(string(output))
 	//
 	Command = fmt.Sprintf("docker rmi %s", imageurl)
-	output, err = exec.Command("cmd", "-c", Command).Output()
+	output, err = Cmd("cmd", "-c", Command).Output()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(string(output))
 }
+
+
+func Cmd(commandName string, params []string) (string, error) {
+    cmd := exec.Command(commandName, params...)
+    fmt.Println("Cmd", cmd.Args)
+    var out bytes.Buffer
+    cmd.Stdout = &out
+    cmd.Stderr = os.Stderr
+    err := cmd.Start()
+    if err != nil {
+        return "", err
+    }
+    err = cmd.Wait()
+    return out.String(), err
