eb5c9b7a6c88ba4cf5eba7c7fa6968957dfdb693
yang <yang186107321@163.com>
2023-01-31 17:16:08 +0800

    修改执行脚本

--- windows-docker/Controller/DockerPush.go
+++ windows-docker/Controller/DockerPush.go
@@ -2,7 +2,6 @@ package Controller
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -24,6 +23,7 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	imageurl := context.Query("imageurl")
 	harborurl := strings.Replace(imageurl, "192.168.48.36", "172.20.4.89:8899", -1)
 	//
+
 	Command := fmt.Sprintf("docker pull %s ", imageurl)
 	Commandarry := []string{Command}
 	output, err := Cmd("cmd", Commandarry)
@@ -70,18 +70,34 @@ func (dockerPush *DockerPush) GetToken(context *gin.Context) {
 	fmt.Println(string(output))
 }
 
-func Cmd(commandName string, params []string) (string, error) {
+func Cmd(commandName string, params []string) error {
 	cmd := exec.Command(commandName, params...)
-	fmt.Println("Cmd", cmd.Args)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = os.Stderr
-	err := cmd.Start()
+	dir := "C:\\Users\\thpower\\Desktop\\push-service"
+	fmt.Println("CmdAndChangeDirToFile", dir, cmd.Args)
+	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return "", err
+		fmt.Println("cmd.StdoutPipe: ", err)
+		return err
+	}
+	cmd.Stderr = os.Stderr
+	cmd.Dir = dir
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
+	reader := bufio.NewReader(stdout)
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Println(line)
 	}
 	err = cmd.Wait()
-	return out.String(), err
+	return err
 }
 
 func CmdAndChangeDirToShow(dir string, commandName string, params []string) error {
